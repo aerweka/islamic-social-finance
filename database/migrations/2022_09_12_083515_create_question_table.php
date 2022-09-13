546df30c13169071c8efed870bfdec2071cc2b22
Dafiq Kurniawan <mdafiq25@gmail.com>
2022-09-13 22:08:43 +0700

    migration abd model

---
+++ database/migrations/2022_09_12_083515_create_question_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuestionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('question', function (Blueprint $table) {
+            $table->increments('id_pertanyaan');
+            $table->integer('id_aspek')->unsigned();
+            $table->integer('kode_indikator');
+            $table->longText('soal');
+            $table->longText('pilihan_1');
+            $table->longText('pilihan_2')->nullable();
+            $table->longText('pilihan_3');
+            $table->longText('pilihan_4')->nullable();
+            $table->longText('pilihan_5');
+            $table->float('bobot_pertanyaan');
+
+            $table->foreign('id_aspek', 'fk_aspek')->references('id_aspek')->on('aspect');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('question');
+    }
+}
