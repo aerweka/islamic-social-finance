4d7fbbff6fe50240604a692a19166473c704864d
rareaqil <rareaqil123@gmail.com>
2022-09-17 14:58:43 +0700

    salah

--- app/Models/answer.php
+++ app/Models/answer.php
@@ -5,23 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class answer extends Model
+class Answer extends Model
 {
     use HasFactory;
-
-    protected $table = 'answer';
-    protected $primaryKey = 'id_jawaban';
-    protected $fillable = [
-        'id',
-        'jawaban_terpilih',
-        'filled_at',
-        'total_env',
-        'total_gov',
-        'total-soc',
-    ];
-
-    public function users()
-    {
-        return $this->belongsTo(users::class, 'id');
-    }
 }
--- app/Models/aspect.php
+++ app/Models/aspect.php
@@ -5,26 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class aspect extends Model
+class Aspect extends Model
 {
     use HasFactory;
-
-    protected $table = 'aspect';
-    protected $primaryKey = 'id_aspek';
-    protected $fillable = [
-        'kode',
-        'aspek',
-        'id_dimensi',
-        'bobot_aspek'
-    ];
-
-    public function dimention()
-    {
-        return $this->belongsTo(dimention::class, 'id_dimensi');
-    }
-
-    public function question()
-    {
-        return $this->hasMany(question::class, 'id_aspek');
-    }
 }
